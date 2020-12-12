2cb78a3c30e0cca7dfb9b60cfec55686a67f42d5
akbar.habeeb <akbar.h@samsung.com>
2020-12-12 23:04:11 +0530

    Suspect to error proned, yet functional code

---
+++ qp-generator-frontend/src/route.js
@@ -0,0 +1,17 @@
+// import Home from './Views/Home'
+import Generate from './Views/AddTextField'
+
+const Routes = [
+  // {
+  //   path: "/",
+  //   name: "home",
+  //   component: Home
+  // }, 
+  {
+    path: "/",
+    name: "createQP",
+    component: Generate
+  }
+];
+
+export default Routes;
