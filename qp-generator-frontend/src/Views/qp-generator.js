8ca80c523775859492936e06b02ebbb3949dfb0b
Akbar <akbar.h@samsung.com>
2020-12-13 13:47:08 +0530

    Typo in file name

--- qp-generator-frontend/src/Views/qp-generator.js
+++ qp-generator-frontend/src/Views/qp-generator.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Col, Row } from 'reactstrap';
-import Generator from '../Containers/Generator'
+import Generator from '../Containers/generator'
 
 class AddTextField extends Component {
   constructor() {
