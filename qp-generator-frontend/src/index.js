da24cf94ca2273d28f7131261514fec3a52aa5d7
RithickB <rithick0907@gmail.com>
2020-12-14 15:25:37 +0530

    Existing Navbar Updated

--- qp-generator-frontend/src/index.js
+++ qp-generator-frontend/src/index.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './Layout/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./Layout/App";
 import { Route, Switch, HashRouter } from "react-router-dom";
 
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(
   <HashRouter>
     <Switch>
-      <Route path="/" render={props => <App {...props} />} />
+      <Route path="/" render={(props) => <App {...props} />} />
     </Switch>
   </HashRouter>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
